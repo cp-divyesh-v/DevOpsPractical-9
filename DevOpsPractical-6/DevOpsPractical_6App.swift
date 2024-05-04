47affd24cb032ccbbad116c0a68c4fd63e34bac8
Divyesh Canopas <divyesh.v@canopas.com>
2024-05-04 11:33:40 +0530

    Initial Commit

---
+++ DevOpsPractical-6/DevOpsPractical_6App.swift
@@ -0,0 +1,17 @@
+//
+//  DevOpsPractical_6App.swift
+//  DevOpsPractical-6
+//
+//  Created by Divyesh Vekariya on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DevOpsPractical_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DevOpsPractical-6UITests/DevOpsPractical_6UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DevOpsPractical_6UITestsLaunchTests.swift
+//  DevOpsPractical-6UITests
+//
+//  Created by Divyesh Vekariya on 04/05/24.
+//
+
+import XCTest
+
+final class DevOpsPractical_6UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
